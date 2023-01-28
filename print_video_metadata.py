15d7faf7a377a5461b980a66847ac2fe5b5f24ad
Mario S. Mira <marioasmira@gmail.com>
2023-01-28 17:53:24 -0500

    Played around with files

--- print_video_metadata.py
+++ print_video_metadata.py
@@ -4,7 +4,7 @@ files = ["test_rename/20211020_171551.mp4"]
 with exiftool.ExifToolHelper() as et:
     metadata = et.get_metadata(files)
 for d in metadata:
-    print("{:20.40} {:20.20}".format(d["SourceFile"],
-                                     d["QuickTime:CreateDate"]))
+    print("{:20.40} {:20.20}".format(d["SourceFile"], d["QuickTime:CreateDate"]))
 
 print(metadata[0]["QuickTime:CreateDate"][:4])
+print(metadata[0]["QuickTime:CreateDate"])
